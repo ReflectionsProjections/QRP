426c852460d153bbda1f4054b615bc5f35e89d56
preetig3 <preetig3@illinois.edu>
2023-06-22 15:34:47 -0700

    new changes

--- reader/ui/login-page.js
+++ reader/ui/login-page.js
@@ -1,22 +1,38 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
+// Check if the user is already logged in
+window.addEventListener("DOMContentLoaded", () => {
+    const email = localStorage.getItem("email");
+    const password = localStorage.getItem("password");
+    if (email && password) {
+      window.location.href = "qr-code.html";
+    }
+  });
+
+
+loginButton.addEventListener("click", async (e) => {
     e.preventDefault();
     const email = loginForm.email.value;
+    const data = { email: email };
+    console.log(data);
+    
+
     try {
-        fetch("http://localhost:8080/generateVerificationPassword", {
+        const response = await fetch("http://dev.reflectionsprojections.org/api/auth/generate", {
             method: "POST",
             mode: 'cors',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(JSON.parse(email)),
-        });    
-        console.log('Success:');
-    } catch (e) {
-        console.log('Error', e);
-    }
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        });
 
-    window.location.href =  "/login-page2.html"
-  
-})
+        if (response.ok) {
+            console.log('Success');
+            window.location.href = "login-page2.html"; // Redirect to login-page2.html
+        } else {
+            console.log('Error:', response.status);
+        }
+    } catch (e) {
+        console.log('Error:', e);
+    }
+});
--- reader/ui/login-page2.js
+++ reader/ui/login-page2.js
@@ -1,24 +1,45 @@
 const loginForm = document.getElementById("login-form2");
 const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
+
+// Check if the user is already logged in
+window.addEventListener("DOMContentLoaded", () => {
+  const email = localStorage.getItem("email");
+  const password = localStorage.getItem("password");
+  if (email && password) {
+    window.location.href = "qr-code.html";
+  }
+});
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const email = loginForm.email.value;
-    const password = loginForm.password.value;
-    try {
-        fetch("http://localhost:8080/verifyPassword", {
-            method: "POST",
-            mode: 'cors',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify(JSON.parse(email, password)),
-        });    
-        console.log('Success:');
-        window.location.href =  "/qr-code.html"
-    } catch (e) {
-        console.log('Error', e);
-    }
+  e.preventDefault();
+  const email = loginForm.email.value;
+  const password = loginForm.password.value;
+  loginUser(email, password);
+});
 
-    
-  
-})
+function loginUser(email, password) {
+  const data = { email: email, passcode: password };
+  try {
+    fetch("http://dev.reflectionsprojections.org/api/auth/verify", {
+      method: "POST",
+      mode: 'cors',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    })
+      .then(response => {
+        if (response.ok) {
+          // Store the credentials in local storage
+          localStorage.setItem("email", email);
+          localStorage.setItem("password", password);
+          window.location.href = "index.html";
+        } else {
+          console.log('Login failed');
+        }
+      })
+      .catch(error => {
+        console.log('Error', error);
+      });
+  } catch (e) {
+    console.log('Error', e);
+  }
+}
